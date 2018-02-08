da4407a1a2d0a17e94862c49d29b31af2e3860bc
Sergey Kalich R3UAL <drhyperkalich@gmail.com>
2018-02-08 16:47:47 +0300

    commit

--- LTN/LTN_contract.sol
+++ LTN/LTN_contract.sol
@@ -262,7 +262,6 @@ contract LTN is ERC20Interface, Owned {
         require(tokens <= balances[msg.sender]);
         require(tokens != 0);
         balances[msg.sender] = balances[msg.sender].sub(tokens);
-        balances[address(0)] = balances[address(0)].add(tokens);
         Transfer(msg.sender, address(0), tokens);
         return true;
     }    
